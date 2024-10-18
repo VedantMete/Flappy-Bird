6788b9af5b0ece92d8138aaa35fd23f22e9fe0c2
VedantMete <vedantmete@gmail.com>
2024-10-18 13:24:12 +0530

    Back-End Update

---
+++ src/FlappyBird.java
@@ -0,0 +1,142 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class FlappyBird extends JPanel implements ActionListener, KeyListener{
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    Image backGroundImg;
+    Image BirdImg;
+    Image TopPipeImg;
+    Image BottomPipeImg;
+
+    int birdx = boardWidth/8;
+    int birdy = boardWidth/2;
+    int birdWidth = 34;
+    int birdHeight = 24;
+
+    class Bird{
+        int x = birdx;
+        int y = birdy;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+        Bird(Image img){
+            this.img=img;
+        }
+    }
+
+    int pipex = boardHeight;
+    int pipey = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
+
+    class Pipe{
+        int x = pipex;
+        int y = pipey;
+        int width = pipeWidth;
+        int height = pipeHeight;
+        Image img;
+        boolean passed = false;
+        Pipe(Image img){
+            this.img = img;
+        }
+    }
+
+    Bird bird;
+    int velocityX = -4;
+    int velocityY = 0;
+    int gravity = 1;
+
+    ArrayList<Pipe> pipes;
+    Random random = new Random();
+
+    Timer gameLoop;
+    Timer placePipeTimer;
+    boolean gameOver;
+    double score = 0;
+
+    FlappyBird(){
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        setFocusable(true);
+        addKeyListener(this);
+
+        backGroundImg = new ImageIcon(getClass().getResource("src/assests/flappybirdbg.png")).getImage();
+        BirdImg = new ImageIcon(getClass().getResource("src/assests/flappybird.png")).getImage();
+        TopPipeImg = new ImageIcon(getClass().getResource("src/assests/toppipe.png")).getImage();
+        BottomPipeImg = new ImageIcon(getClass().getResource("src/assests/bottompipe.png")).getImage();
+
+        bird = new Bird(BirdImg);
+        pipes = new ArrayList<Pipe>();
+
+        placePipeTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+        placePipeTimer.start();
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
+    }
+
+    void placePipes(){
+        int randomPipeY = (int) (pipey - pipeHeight/4 - Math.random()*(pipeHeight/2));
+        int openingSpace = boardHeight/4;
+        Pipe topPipe = new Pipe(TopPipeImg);
+        topPipe.y=randomPipeY;
+        pipes.add(topPipe);
+        Pipe bottomPipe = new Pipe(BottomPipeImg);
+        bottomPipe.y = topPipe.y + pipeHeight + openingSpace;
+        pipes.add(bottomPipe);
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g){
+        g.drawImage(backGroundImg, 0, 0, this.boardWidth, this.boardHeight, null);
+        g.drawImage(BirdImg, bird.x, bird.y, bird.width, bird.height, null);
+        for(int i = 0; i<pipes.size();i++){
+            Pipe pipe = pipes.get(i);
+            g.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height, null);
+        }
+
+        g.setColor(Color.white);
+        g.setFont(new Font("Dialog", Font.PLAIN,32));
+        if(gameOver){
+            g.drawString("Game Over BKL: " + String.valueOf((int) score),10,35);
+        }
+        else{
+            g.drawString(String.valueOf((int)score), 10, 25);
+        }
+    }
+
+    public void move(){
+        velocityY = velocityY + gravity;
+        bird.y = bird.y + velocityY;
+        bird.y = Math.max(bird.y, 0);
+
+        for(int i = 0; i<pipes.size();i++){
+            Pipe pipe = pipes.get(i);
+            pipe.x = pipe.x + velocityX;
+            if(!pipe.passed && bird.x > pipe.x + pipe.width){
+                score = score + 0.5;
+                pipe.passed = true;
+            }
+            if(collision(bird, pipe)){
+                gameOver = true;
+            }
+        }
+        if(bird.y > boardHeight){
+            gameOver = true;
+        }
+    }
+    if(bird.y > boardHeight){
+        gameOver = true;
+    }
+
+}
